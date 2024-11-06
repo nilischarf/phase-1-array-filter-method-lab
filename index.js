5615524fe26c9292e63ccdff16e650d5834f56ad
Nili Scharf <nilischarf@gmail.com>
2024-11-06 17:04:06 +0200

    complete array filter method lab

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Code your solution here
+function findMatching(array, name){
+    return array.filter(function(driver) {
+        return driver.toLowerCase() === name.toLowerCase()
+    })
+}
+
+
+function fuzzyMatch(array, name){
+    return array.filter(function(driver) {
+        return driver[0] === name[0]
+    })
+}
+
+
+function matchName(array, name){
+    return array.filter(function(driver) {
+        return driver.name === name
+    })
+}
